0ac8e4112869ab3ccc2f5b827602e93e9318ecad
Alejandro Leonian <alejandromleonian@gmail.com>
2023-11-03 18:58:13 -0500

    prior to MUI

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 export function Body() {
-    function processInput(event) {
-
+    function processInput() {
+        const name = document.getElementById('userName').value;
+        console.log(name);
     }
     return (
         <div className="App-Body">
-            <input type="text" oncChange={processInput} placeholder="Enter your name" />
+            <input type="text" id="userName" placeholder="Enter your name" />
+            <button onClick={processInput}>submit</button>
         </div>
     )
 }
