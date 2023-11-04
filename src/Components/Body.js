aa95ff69c5494cb289e6a088253d1a0c9562a499
Alejandro Leonian <alejandromleonian@gmail.com>
2023-11-04 11:49:58 -0500

    input validation ok

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -2,11 +2,23 @@ import * as React from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
-import { FormControl, FormLabel } from '@mui/material';
+import { FormControl } from '@mui/material';
+import { MyAlert } from './MyAlert';
 
 export function Body() {
+
+    const [emptyUsernameAlert, setAlert] = React.useState(false);
+
+    function handleTextInputChange() {
+        if (emptyUsernameAlert) {
+            setAlert(false);
+        }
+    }
     function processInput() {
         const name = document.getElementById('userName').value;
+        if (!name || name.length == 0) {
+            setAlert(true)
+        }
     }
     var height = 80;
 
@@ -15,9 +27,12 @@ export function Body() {
         <div className="App-Body">
             <FormControl>
                 <Box width={350}>
-                    <TextField style={{ height }} fullWidth rows={30} id="outlined-basic" size="big" label="Enter name" variant="outlined" />
+                    <TextField style={{ height }} onChange={handleTextInputChange} fullWidth rows={30} id="userName" size="big" label="Enter name" variant="outlined" />
                 </Box>
                 <Button variant="contained" onClick={processInput}>submit</Button>
+                <br />
+                {emptyUsernameAlert ? <MyAlert severity="error" message="You must input something 🥱" /> : ""}
+
             </FormControl>
 
         </div>
